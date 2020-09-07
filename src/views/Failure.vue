18b895bec9d3d34ffcf493b7dd2c19427bde373b
郑志宇 <kczssd@gmail.com>
2020-09-07 23:29:41 +0800

    add:Failure.vue

---
+++ src/views/Failure.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="Failure">
+        <failure />
+    </div>
+</template>
+
+<script>
+    import failure from '@/components/failure.vue';
+    export default {
+        name: 'Failure',
+        components: {
+            failure,
+        },
+    };
+</script>
+
+<style>
+</style>
